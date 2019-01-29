4b275065f78f7e0fbcdc09115cca2891fbc4a85f
Jordan Bienvenue <jord.bienvenue@gmail.com>
2019-01-29 12:10:08 +0400

    Jordan: updated universiTy listing

--- web/app/src/components/homepage/devcon-stats.vue
+++ web/app/src/components/homepage/devcon-stats.vue
@@ -1,11 +1,105 @@
 <template>
-  <div class="home-component devcon-stats-wrapper">
+  <div class="home-component devcon-stats-wrapper"> 
+      <h2 class="mega-rainbow">University</h2>
+
     <div class="container">
-      <div class="devcon-stats-container">
-        <div class="stat-wrapper" v-if="getStats" v-for="(stats, index) in getStats"
-          :key="index">
-          <div class="number">{{ stats.number }}</div>
-          <div class="label">{{ stats.name }}</div>
+      
+      <div class="university-listing">
+        <div class="uni-wrapper">
+          <div class="uni-logo">
+          <img src="../../assets/menu-hover.svg" alt="">
+          </div>
+          <h3 class="uni-name">Testing - Name</h3>
+        </div>
+        <div class="uni-wrapper">
+          <div class="uni-logo">
+          <img src="../../assets/menu-hover.svg" alt="">
+          </div>
+          <h3 class="uni-name">Testing - Name</h3>
+        </div>
+        <div class="uni-wrapper">
+          <div class="uni-logo">
+          <img src="../../assets/menu-hover.svg" alt="">
+          </div>
+          <h3 class="uni-name">Testing - Name</h3>
+        </div>
+        <div class="uni-wrapper">
+          <div class="uni-logo">
+          <img src="../../assets/menu-hover.svg" alt="">
+          </div>
+          <h3 class="uni-name">Testing - Name</h3>
+        </div>
+        <div class="uni-wrapper">
+          <div class="uni-logo">
+          <img src="../../assets/menu-hover.svg" alt="">
+          </div>
+          <h3 class="uni-name">Testing - Name</h3>
+        </div>
+        <div class="uni-wrapper">
+          <div class="uni-logo">
+          <img src="../../assets/menu-hover.svg" alt="">
+          </div>
+          <h3 class="uni-name">Testing - Name</h3>
+        </div>
+        <div class="uni-wrapper">
+          <div class="uni-logo">
+          <img src="../../assets/menu-hover.svg" alt="">
+          </div>
+          <h3 class="uni-name">Testing - Name</h3>
+        </div>
+        <div class="uni-wrapper">
+          <div class="uni-logo">
+          <img src="../../assets/menu-hover.svg" alt="">
+          </div>
+          <h3 class="uni-name">Testing - Name</h3>
+        </div>
+        <div class="uni-wrapper">
+          <div class="uni-logo">
+          <img src="../../assets/menu-hover.svg" alt="">
+          </div>
+          <h3 class="uni-name">Testing - Name</h3>
+        </div>
+        <div class="uni-wrapper">
+          <div class="uni-logo">
+          <img src="../../assets/menu-hover.svg" alt="">
+          </div>
+          <h3 class="uni-name">Testing - Name</h3>
+        </div>
+        <div class="uni-wrapper">
+          <div class="uni-logo">
+          <img src="../../assets/menu-hover.svg" alt="">
+          </div>
+          <h3 class="uni-name">Testing - Name</h3>
+        </div>
+        <div class="uni-wrapper">
+          <div class="uni-logo">
+          <img src="../../assets/menu-hover.svg" alt="">
+          </div>
+          <h3 class="uni-name">Testing - Name</h3>
+        </div>
+        <div class="uni-wrapper">
+          <div class="uni-logo">
+          <img src="../../assets/menu-hover.svg" alt="">
+          </div>
+          <h3 class="uni-name">Testing - Name</h3>
+        </div>
+        <div class="uni-wrapper">
+          <div class="uni-logo">
+          <img src="../../assets/menu-hover.svg" alt="">
+          </div>
+          <h3 class="uni-name">Testing - Name</h3>
+        </div>
+        <div class="uni-wrapper">
+          <div class="uni-logo">
+          <img src="../../assets/menu-hover.svg" alt="">
+          </div>
+          <h3 class="uni-name">Testing - Name</h3>
+        </div>
+        <div class="uni-wrapper">
+          <div class="uni-logo">
+          <img src="../../assets/menu-hover.svg" alt="">
+          </div>
+          <h3 class="uni-name">Testing - Name</h3>
         </div>
       </div>
     </div>
@@ -13,77 +107,77 @@
 </template>
 
 <script>
-import { mapGetters, mapActions } from "vuex";
-
-import { FETCH_STATS } from "@/store";
-
-export default {
-  created() {
-    this.FETCH_STATS();
-  },
-  computed: {
-    ...mapGetters(["getStats"])
-  },
-
-  methods: {
-    ...mapActions([FETCH_STATS])
-  }
-};
+// import { mapGetters, mapActions } from "vuex";
+// 
+// import { FETCH_STATS } from "@/store";
+// 
+// export default {
+  // created() {
+    // this.FETCH_STATS();
+  // },
+  // computed: {
+    // ...mapGetters(["getStats"])
+  // },
+// 
+  // methods: {
+    // ...mapActions([FETCH_STATS])
+  // }
+// };
 </script>
 
 <style lang="scss" scoped>
 .devcon-stats-wrapper {
   padding: 100px 0;
 }
-.devcon-stats-container {
+.university-listing {
   display: grid;
   // justify-content: space-around;
   grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
   grid-row-gap: 100px;
 
-  .stat-wrapper {
+  .uni-wrapper {
     position: relative;
     padding: 30px 0 0 0;
     --width: 200px;
     width: var(--width);
 
-    &:before {
-      content: " ";
-      width: var(--width);
-      height: 140px;
-      // left: 50%;
-      top: 0px;
-      left: 0;
-      margin-left: -calc(var(--width) / 2);
+    // &:before {
+    //   content: " ";
+    //   width: var(--width);
+    //   height: 140px;
+    //   // left: 50%;
+    //   top: 0px;
+    //   left: 0;
+    //   margin-left: -calc(var(--width) / 2);
 
-      position: absolute;
-      background-image: url("../../assets/menu-hover.svg");
-      background-repeat: no-repeat;
-      background-position: center center;
-      background-size: contain;
-      transform: translate(0px, 10px);
-      opacity: 0.5;
-      transition: all 0.5s ease-out;
-    }
-    &:after {
-      content: " ";
-      --width: 100px;
-      width: var(--width);
-      height: 40px;
-      left: calc(var(--width) * 1.2);
-      top: 0px;
-      // left: 0;
-      // margin-left: -calc(var(--width) / 2);
+    //   position: absolute;
+    //   background-image: url("../../assets/menu-hover.svg");
+    //   background-repeat: no-repeat;
+    //   background-position: center center;
+    //   background-size: contain;
+    //   transform: translate(0px, 10px);
+    //   opacity: 0.5;
+    //   transition: all 0.5s ease-out;
+    // }
+    // &:after {
+    //   content: " ";
+    //   --width: 100px;
+    //   width: var(--width);
+    //   height: 40px;
+    //   left: calc(var(--width) * 1.2);
+    //   top: 0px;
+    //   // left: 0;
+    //   // margin-left: -calc(var(--width) / 2);
 
-      position: absolute;
-      background-image: url("../../assets/menu-hover.svg");
-      background-repeat: no-repeat;
-      background-position: center center;
-      background-size: contain;
-      transform: translate(0px, 10px);
-      opacity: 0.5;
-      transition: all 0.5s ease-out;
-    }
+    //   position: absolute;
+    //   background-image: url("../../assets/menu-hover.svg");
+    //   background-repeat: no-repeat;
+    //   background-position: center center;
+    //   background-size: contain;
+    //   transform: translate(0px, 10px);
+    //   opacity: 0.5;
+    //   transition: all 0.5s ease-out;
+    // }
 
     .number {
       font-size: 60px;
